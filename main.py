247373e6c0e13137ccb01b4bc359cd407ab57382
Norbert-SCH <norbert.schwankhaus@saint-gobain.com>
2025-08-22 07:49:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,21 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-RESULT_FOLDER = 'results'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(RESULT_FOLDER, exist_ok=True)
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file.filename.endswith('.xls'):
-            filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+            # Speichere die Datei im temporären Verzeichnis
+            filepath = os.path.join('/tmp', file.filename)
             file.save(filepath)
 
             # Verarbeitung: Extrahiere Zelle B40
             df = pd.read_excel(filepath, header=None, engine='xlrd')
             b40_value = df.iloc[39, 1]  # B40 = Zeile 40, Spalte 2 (Index 39, 1)
 
-            # Ergebnisdatei erstellen
-            result_path = os.path.join(RESULT_FOLDER, 'result.xlsx')
+            # Ergebnisdatei im temporären Verzeichnis erstellen
+            result_path = os.path.join('/tmp', 'result.xlsx')
             result_df = pd.DataFrame({'B40': [b40_value]})
             result_df.to_excel(result_path, index=False)
 
